5fed5c8cbcaaa59ec17094af672c9ced0591a9cb
Santanu Biswas <69161246+Santanu29@users.noreply.github.com>
2021-07-28 21:29:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const countdown = () => {
+    const countDate = new Date("November 11, 2021 00:00:00").getTime();
+    const now = new Date().getTime();
+    const diff = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(diff / day);
+    const textHour = Math.floor((diff % day) / hour);
+    const textMinute = Math.floor((diff % hour) / minute);
+    const textSecond = Math.floor((diff % minute) / second);
+
+    document.querySelector(".day").innerText = textDay;
+    document.querySelector(".hour").innerText = textHour;
+    document.querySelector(".minute").innerText = textMinute;
+    document.querySelector(".second").innerText = textSecond;
+};
+
+setInterval(countdown, 1000);
+
+
